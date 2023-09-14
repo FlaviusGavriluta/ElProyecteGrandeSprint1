d2aa4b3ea3fd4ddeabf910f7c2359d3916328056
Daniel <dani.marinescu7@gmail.com>
2023-09-14 20:13:27 +0300

    Project initializer

---
+++ src/test/java/com/codecool/onlineshop/OnlineshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
